3df214578f0a5a0ce1ab46800bffee3bf36d9f75
kevin-rodri <kr399880@gmail.com>
2025-02-05 20:10:29 -0500

    fixed issue

--- src/components/patient-history/PatientHistoryComponent.jsx
+++ src/components/patient-history/PatientHistoryComponent.jsx
@@ -1,114 +1,121 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
-    Box,
-    Button,
-    Card,
-    CircularProgress,
-    FormControl,
-    FormGroup,
-    FormLabel,
-    TextField,
-    Typography,
-    Snackbar,
-    Alert,
-    Paper,
-    Fab,
-    TableRow,
-    List,
-    ListItem,
-    ButtonGroup
-  } from "@mui/material";
-  import { Delete, Edit } from "@mui/icons-material";
-  import { Add } from "@mui/icons-material";
-  import { useForm } from "react-hook-form";
-  import { useNavigate } from "react-router-dom";
-import { getPatientById } from "../../services/patientService";
-import { getPatientHistory, updatePatientHistory } from "../../services/patientHistoryService";
+  Box,
+  Button,
+  Card,
+  CircularProgress,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  TextField,
+  Typography,
+  Snackbar,
+  Alert,
+  Paper,
+  Fab,
+  TableRow,
+  List,
+  ListItem,
+  ButtonGroup,
+} from "@mui/material";
+import { Delete, Edit, Add } from "@mui/icons-material";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import PatientHistoryModalComponent from "./PatientHistoryModalComponent";
 
-  export default function PatientHistoryComponent() {
+export default function PatientHistoryComponent() {
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const {
-        handleSubmit,
-        formState: { errors },
-      } = useForm();
+  // controlling the open/close of the modal:
+  const [openModal, setOpenModal] = useState(false);
 
-    /*  useEffect(() => {
-          const fetchHistory = async () => {
-            try {
-              const data = await getPatientHistory();
-              setHistory(data);
-            } catch (error) {
-              throw error;
-            }
-          };
-          fetchHistory();
-        }, []); */
+  const patientHistories = [
+    {
+      type: "Primary Admitting Diagnosis",
+      title: "Patient's Primary Admitting Diagnosis",
+      descrption: "This is Patients Primary Admitting Diagnosis",
+    },
+    {
+      type: "Family Health History",
+      title: "Patient's Family Health History",
+      descrption: "This is Patient's Family Health History",
+    },
+    {
+      type: "Social History",
+      title: "Patient's Social History",
+      descrption: "This is Patient's Social History",
+    },
+    {
+      type: "Medical/Sugical History",
+      title: "Patient's Medical/Sugical History",
+      descrption: "This is Patient's Medical/Sugical History",
+    },
+  ];
 
-    const [modal, setModal] = useState(false);
-    const [histories, setHistories] = useState([]);
+  return (
+    <Box>
+      <PatientHistoryModalComponent
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          padding: 5,
+          flexDirection: "column",
+          alignItems: "flex-start",
+          backgroundColor: "white",
+          marginBottom: 10,
+          marginLeft: 20,
+          marginRight: 30,
+          borderRadius: 3,
+        }}
+      >
+        <TableRow>
+          <FormLabel>History Title</FormLabel>
+          <FormLabel sx={{ marginLeft: 25 }}>Orders</FormLabel>
+          <Fab
+            aria-label="add"
+            sx={{ marginLeft: 100 }}
+            onClick={() => setOpenModal(true)}
+          >
+            <Add />
+          </Fab>
+        </TableRow>
+        <List>
+          {patientHistories.map((patientHistory, index) => (
+            <ListItem key={index}>
+              {patientHistory.type}
+              <span style={{ marginLeft: 20 }}>
+                {patientHistory.descrption}
+              </span>
+              <ButtonGroup sx={{ marginLeft: 2 }}>
+                <Fab>
+                  <Edit />
+                </Fab>
+                <Fab>
+                  <Delete />
+                </Fab>
+              </ButtonGroup>
+            </ListItem>
+          ))}
+        </List>
+        <Button
+          sx={{
+            display: "flex",
+            width: 143,
+            height: 49,
+          }}
+          variant="contained"
+        >
+          Save
+        </Button>
+      </Box>
 
-    const patientHistories = [
-        {
-            type: "Primary Admitting Diagnosis",
-            title: "Patient's Primary Admitting Diagnosis",
-            descrption: "This is Patients Primary Admitting Diagnosis"
-        },
-        {
-            type: "Family Health History",
-            title: "Patient's Family Health History",
-            descrption: "This is Patient's Family Health History"
-        },
-        {
-            type: "Social History",
-            title: "Patient's Social History",
-            descrption: "This is Patient's Social History"
-        },
-        {
-            type: "Medical/Sugical History",
-            title: "Patient's Medical/Sugical History",
-            descrption: "This is Patient's Medical/Sugical History"
-        }
-    ]
-
-    return(
-
-        <Box>
-            <Box sx={{
-                display: "flex",
-                padding: 5,
-                flexDirection: "column",
-                alignItems: "flex-start",
-                backgroundColor: "white",
-                marginBottom: 10,
-                marginLeft: 20,
-                marginRight: 30,
-                borderRadius: 3
-            }}>
-                <TableRow>
-                        <FormLabel>History Title</FormLabel>
-                        <FormLabel sx={{marginLeft: 25}}>Orders</FormLabel>
-                    <Fab aria-label="add" sx={{marginLeft: 100}}><Add /></Fab>
-                </TableRow>
-                <List>
-                    {patientHistories.map((patientHistory, index) =>
-                    <ListItem>
-                        {patientHistory.type}
-                        <text sx={{marginLeft: 20}}>{patientHistory.descrption}</text>
-                        <ButtonGroup>
-                        <Fab><Edit/></Fab>
-                        <Fab><Delete/></Fab>
-                        </ButtonGroup>
-                    </ListItem>
-                    )}
-                </List>
-                <Button sx={{
-                    display: "flex",
-                    width: 143,
-                    height: 49
-                }} variant="contained">Save</Button>
-            </Box>
-            <PatientHistoryModalComponent />
-        </Box>
-    );
-  }
+      {/* Pass the open and onClose props down to your modal component */}
+    </Box>
+  );
+}
--- src/components/patient-history/PatientHistoryModalComponent.jsx
+++ src/components/patient-history/PatientHistoryModalComponent.jsx
@@ -1,85 +1,95 @@
-import { Button, CardActions, CardContent, CardHeader, FormControl, MenuItem, Modal, Select, TextField } from "@mui/material";
-import React, { useState, useForm } from "react";
-import ReactModal from "react";
-import { addPatientHistory } from "../../services/patientHistoryService";
-import { getPatientById } from "../../services/patientService";
-
-export default function PatientHistoryModalComponent() {
-
-   /* const {
-            handleSubmit,
-            formState: { errors },
-            setValue,
-          } = useForm();*/
-
-    const [title, setTitle] = useState(null);
-    const [description, setDescription] = useState(null);
-    const [historyType, setHistoryType] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [usable, setUsable] = useState(false);
-
-   /* const onSubmit = async (data) => {
-        
-    } */
-
-
-    function handleTitle(event){
-        setTitle(event.target.value);
+import { 
+    Button, 
+    CardActions, 
+    CardContent, 
+    CardHeader, 
+    FormControl, 
+    MenuItem, 
+    Modal, 
+    Select, 
+    TextField 
+  } from "@mui/material";
+  import React, { useState } from "react";
+  
+  
+  export default function PatientHistoryModalComponent({ open, onClose }) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [historyType, setHistoryType] = useState("");
+  
+    function handleTitle(event) {
+      setTitle(event.target.value);
     }
-
-    function handleDescription(event){
-        setTitle(event.target.value);
+  
+    function handleDescription(event) {
+      setDescription(event.target.value);
     }
-
-    function handleHistoryType(event){
-        setTitle(event.target.value);
-    } 
-
-
-    return(
-
-        <div>
-            <Modal sx={{
-                display: "flex",
-                width: 809,
-                height: 793,
-                flexDirection: "column",
-                alignItems: "flex-start"
-            }}>
-                <CardHeader sx={{
-                    display: "flex",
-                    padding: 16,
-                    alignItems: "center",
-                    alignSelf: "stretch"
-                }}>
-                    Add Patient History
-                </CardHeader>
-                <CardContent>
-                    <div>
-                        <text>Title</text>
-                        <TextField /*value={title} onSubmit={handleTitle}*//>
-                    </div>
-                    <div>
-                        <text>Description</text>
-                        <TextField rows={4} /*value={description} onSubmit={handleDescription}*//>
-                    </div>
-                    <div>
-                        <text>History Type</text>
-                        <FormControl>
-                        <Select /*value={historyType} onSubmit={handleHistoryType}*/>
-                            <MenuItem value={"Primary Admitting Diagnosis"}>Primary Admitting Diagnosis</MenuItem>
-                            <MenuItem value={"Family Heath History"}>Family Heath History</MenuItem>
-                            <MenuItem value={"Social History"}>Social History</MenuItem>
-                            <MenuItem value={"Medical/Surgical History"}>Medical/Surgical History</MenuItem>
-                        </Select>
-                        </FormControl>
-                    </div>
-                </CardContent>
-                <CardActions>
-                    <Button color="red">Cancel</Button>
-                    <Button color="blue">Save</Button>
-                </CardActions>
-            </Modal>
+  
+    function handleHistoryType(event) {
+      setHistoryType(event.target.value);
+    }
+  
+    return (
+      <Modal 
+        open={open} 
+        onClose={onClose} 
+        sx={{
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          marginTop: "50px"
+        }}
+      >
+        <div style={{ background: "#fff", padding: 16 }}>
+          <CardHeader 
+            sx={{
+              display: "flex",
+              padding: 16,
+              alignItems: "center",
+              alignSelf: "stretch"
+            }}
+            title="Add Patient History"
+          />
+          <CardContent>
+            <div>
+              <label>Title</label>
+              <TextField 
+                value={title}
+                onChange={handleTitle} 
+                fullWidth
+              />
+            </div>
+            <div style={{ marginTop: 16 }}>
+              <label>Description</label>
+              <TextField
+                value={description} 
+                onChange={handleDescription}
+                rows={4}
+                multiline 
+                fullWidth
+              />
+            </div>
+            <div style={{ marginTop: 16 }}>
+              <label>History Type</label>
+              <FormControl fullWidth>
+                <Select 
+                  value={historyType}
+                  onChange={handleHistoryType}
+                >
+                  <MenuItem value={"Primary Admitting Diagnosis"}>Primary Admitting Diagnosis</MenuItem>
+                  <MenuItem value={"Family Heath History"}>Family Heath History</MenuItem>
+                  <MenuItem value={"Social History"}>Social History</MenuItem>
+                  <MenuItem value={"Medical/Surgical History"}>Medical/Surgical History</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button color="secondary" onClick={onClose}>Cancel</Button>
+            <Button color="primary">Save</Button>
+          </CardActions>
         </div>
+      </Modal>
     );
   }
+  
